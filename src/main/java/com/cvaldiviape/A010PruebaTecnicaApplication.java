979b0229cbcf03a57c409082772ca5ee768b179f
cvaldiviape <cvaldiviape@gmail.com>
2022-10-24 23:46:19 -0500

    added entities

--- src/main/java/com/cvaldiviape/A010PruebaTecnicaApplication.java
+++ src/main/java/com/cvaldiviape/A010PruebaTecnicaApplication.java
@@ -2,8 +2,12 @@ package com.cvaldiviape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableTransactionManagement
 public class A010PruebaTecnicaApplication {
 
 	public static void main(String[] args) {
