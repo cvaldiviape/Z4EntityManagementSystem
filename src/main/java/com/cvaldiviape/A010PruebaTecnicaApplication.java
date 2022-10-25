c337bac2125c04091a629d96b249616f0f90fda6
cvaldiviape <cvaldiviape@gmail.com>
2022-10-24 19:58:23 -0500

    initial

---
+++ src/main/java/com/cvaldiviape/A010PruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.cvaldiviape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A010PruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A010PruebaTecnicaApplication.class, args);
+	}
+
+}
